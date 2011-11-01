ce22c00c865ad42e342648b4e0840552d847c1da
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2011-11-01 18:55:57 +0100

    asserv

--- simu/define.py
+++ simu/define.py
@@ -9,3 +9,13 @@ COLLTYPE_DEFAULT	= 0
 class EngineObject:
 	def step(self, dt):
 		pass
+
+
+def mm_to_px(pos):
+	x,y = pos
+	return x/4.0, y/4.0
+
+def px_to_mm(pos):
+	x,y = pos
+	return x*4.0,y*4.0
+
