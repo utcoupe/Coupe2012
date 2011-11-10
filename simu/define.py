7126b10ab6bec0299a0444d57d6ea277845cb8f4
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2011-11-10 17:19:19 +0100

    ajout d'un doctest

--- simu/define.py
+++ simu/define.py
@@ -22,9 +22,25 @@ def mm_to_px(*args):
 	"""
 	@param args une liste ou un atom de valeurs ou de positions
 	@param la veleur ou la liste convertie
+
+	>>> mm_to_px(4) == int(4/PX_TO_MM)
+	True
+
+	>>> mm_to_px((4,8)) == (mm_to_px(4),mm_to_px(8))
+	True
+
+	>>> mm_to_px((4,8),(16,32)) == ((mm_to_px(4),mm_to_px(8)), (mm_to_px(16),mm_to_px(32)))
+	True
+
+	>>> mm_to_px([(4,8),(16,32)]) == mm_to_px((4,8),(16,32))
+	True
+
+	>>> mm_to_px(*[(4,8),(16,32)]) == mm_to_px((4,8),(16,32))
+	True
+	
 	"""
 	def f(a):
-		if type(a) == tuple:
+		if type(a) == tuple or type(a) == list:
 			return mm_to_px(*a)
 		else:
 			return int(a / PX_TO_MM)
@@ -39,7 +55,7 @@ def px_to_mm(*args):
 	@param la veleur ou la liste convertie
 	"""
 	def f(a):
-		if type(a) == tuple:
+		if type(a) == tuple or type(a) == list:
 			return mm_to_px(*a)
 		else:
 			return int(a * PX_TO_MM)
@@ -50,3 +66,11 @@ def px_to_mm(*args):
 
 
 CANAL_ASSERV		= "#asserv"
+
+
+if __name__ == "__main__":
+	import doctest
+	doctest.testmod()
+
+
+
