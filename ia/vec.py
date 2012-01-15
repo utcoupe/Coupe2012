0fb2eedf3e130b4eba1eb5b53a25d4c672f796df
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2012-01-15 22:35:02 +0100

    ajout doctest + reverse operators

--- ia/vec.py
+++ ia/vec.py
@@ -11,6 +11,9 @@ class Vec(list):
 		else:
 			return Vec(( x+y for x,y in zip(self,v2)))
 
+	def __radd__(self, v2):
+		return self + v2
+	
 	def __iadd__(self, v2):
 		if len(self) != len(v2):
 			raise Exception()
@@ -18,13 +21,16 @@ class Vec(list):
 			for i,y in enumerate(v2):
 				self[i] += y
 		return self
-		
+	
 	def __sub__(self, v2):
 		if len(self) != len(v2):
 			raise Exception()
 		else:
 			return Vec(( x-y for x,y in zip(self,v2)))
 
+	def __rsub__(self, v2):
+		return Vec(v2) - self
+	
 	def __isub__(self, v2):
 		if len(self) != len(v2):
 			raise Exception()
@@ -36,6 +42,9 @@ class Vec(list):
 	def __mul__(self, a):
 		return Vec(( a*x for x in self ))
 
+	def __rmul__(self, a):
+		return self * a
+
 	def __imul__(self, a):
 		for i,y in enumerate(self):
 			self[i] *= a
@@ -44,6 +53,9 @@ class Vec(list):
 	def __div__(self, a):
 		return Vec(( x/a for x in self ))
 
+	def __rdiv__(self, a):
+		return self / a
+
 	def __idiv__(self, a):
 		for i,x in enumerate(self):
 			self[i] /= a
@@ -51,6 +63,9 @@ class Vec(list):
 
 	def __truediv__(self, a):
 		return Vec(( x/a for x in self ))
+
+	def __rtruediv__(self, a):
+		return self / a
 
 	def __itruediv__(self, a):
 		for i,x in enumerate(self):
