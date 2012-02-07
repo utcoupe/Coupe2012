39cbaa99a8066ee4c2474a19e55697e68723248c
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2012-02-07 19:00:09 +0100

    ajout de is_clockwise et de set_cw & set_ccw

--- lib/geometry/convexpoly.py
+++ lib/geometry/convexpoly.py
@@ -1,12 +1,29 @@
 
 
 from poly import Poly
+from segment import *
 
 
 class ConvexPoly(Poly):
+	def __init__(self, points):
+		Poly.__init__(self, points)
+		
+	def set_cw(self):
+		if not self.is_clockwise():
+			self.points.reverse()
+
+	def set_ccw(self):
+		if self.is_clockwise():
+			self.points.reverse()
+	
+	def is_clockwise(self):
+		ab = Segment(self.points[0], self.points[1])
+		c = self.points[2]
+		return ab.pos_point(c) < 0
+		
 	def __contains__(self, o):
 		"""
-			Le polygon doit être défini dans le sens des aiguilles d'une montre
+			Le polygon doit être défini dans le sens trigo
 		"""
 		if o in self.AABB:
 			for edge in self.iedges():
