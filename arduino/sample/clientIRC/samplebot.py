6b21fefd381d7d99a8db132f5efccd9bcc617e64
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2012-03-05 06:31:46 +0100

    basic bot

---
+++ arduino/sample/clientIRC/samplebot.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+
+import sys
+import os
+FILE_DIR  = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(FILE_DIR,"..","..","clientIRC"))
+
+import arduinobot
+
+if __name__ == "__main__":
+	arduinobot.run(
+		nickname="samplebot",
+		channel="#test",
+		serial_port="/dev/ttyACM0",
+		protocole_file=os.path.join(FILE_DIR,"..","driver","protocole.h"),
+		protocole_prefixe="Q_")
---
+++ arduino/sample/driver/driver.pde
@@ -0,0 +1,32 @@
+#include "command.h"
+
+void setup()
+{
+	initSerialLink();
+	for (int i=2; i<14; ++i)
+	{
+		pinMode(i, OUTPUT);
+	}
+}
+
+long long int timeStart;
+void loop()
+{
+	timeStart = micros();
+	// La del est allumee pendant le traitement
+
+	
+	readIncomingData();
+
+	
+
+
+	
+	// On attend le temps qu'il faut pour boucler
+	long udelay = 2000-(micros()-timeStart);
+	if(udelay>0)
+		delayMicroseconds(udelay);
+	
+}
+
+
