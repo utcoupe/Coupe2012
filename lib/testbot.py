703d7d4b3d4dbc23957dc0ad789e59be7da4fdd2
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2012-03-07 03:16:42 +0100

    modification du test bot

--- lib/testbot.py
+++ lib/testbot.py
@@ -1,5 +1,19 @@
 
 
-from py3irc.py3irc import testbot
+from py3irc.mypyirc import MyPyIrcBot
 
-testbot.main()
+
+
+class TestBot(MyPyIrcBot):
+	def __init__(self,serverip, serverport):
+		MyPyIrcBot.__init__(self, serverip, serverport, "testbot", ["#test"])
+		self.canal = "#test"
+
+	def cmd_test_test(self, msg, **kwargs):
+		self.send(self.canal, msg)
+		return msg
+
+
+if __name__ == "__main__":
+	bot = TestBot("grosordi.cheuv.eu", 6667)
+	bot.start()
