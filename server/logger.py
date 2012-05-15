28e744e5cea5f0017c4ce310dd7ee3646e4d79b0
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2012-05-15 23:39:17 +0200

    bug fix du logger du server

--- server/logger.py
+++ server/logger.py
@@ -2,17 +2,17 @@
 
 
 import sys
-TERMINAL = sys.stdout
 
 class Logger:
 	def __init__(self, file_path):
-		self.terminal = TERMINAL
+		self.terminal = sys.stdout
 		self.log = open(file_path, 'a')
 
 	def flush(self):
 		self.terminal.flush()
+		self.log.flush()
 	
 	def write(self, msg):
 		self.terminal.write(msg)
-		self.terminal.flush()
 		self.log.write(msg)
+		self.flush()
